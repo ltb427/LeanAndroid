74ced68cd75d59ae68587030264bd2a85366e8c9
ltb427 <1123039786@qq.com>
2019-06-21 22:19:30 +0800

    Menu的使用：     1.在资源文件中创建menu文件夹，在menu文件夹内创建main.xml menu资源文件     2.在活动代码中，重载       public boolean onCreateOptionsMenu(Menu menu)       {       	getMenuInflater().inflate(R.menu.main,menu);         return true;       }       public boolean onOptionsItemSelected(MenuItem item)函数。

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -3,6 +3,8 @@ package com.example.activitytest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,5 +27,31 @@ public class FirstActivity extends AppCompatActivity
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(FirstActivity.this, "you clicked add",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FirstActivity.this, "you clicked remove",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
